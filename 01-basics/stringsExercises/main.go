0b149a04ca10389b5665803eb9c0ca5cb840c79f
Brendan Ashton <brendan.ashton@getweave.com>
2022-03-08 08:14:47 -0700

    finish string exercises

--- 01-basics/stringsExercises/main.go
+++ 01-basics/stringsExercises/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+	"unicode/utf8"
 )
 
 func main() {
 	literal()
 	printJson()
 	rawConcat()
+	countChars()
+	improvedBanger()
+	trimIt()
+	rightTrim()
 }
 
 // ---------------------------------------------------------
@@ -114,4 +120,98 @@ func rawConcat() {
 how are you?`
 
 	fmt.Println(msg)
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Count the Chars
+//
+//  1. Change the following program to work with unicode
+//     characters.
+//
+// INPUT
+//  "İNANÇ"
+//
+// EXPECTED OUTPUT
+//  5
+// ---------------------------------------------------------
+
+func countChars() {
+	// Currently it returns 7
+	// Because, it counts the bytes...
+	// It should count the runes (codepoints) instead.
+	//
+	// When you run it with "İNANÇ", it should return 5 not 7.
+
+	name := os.Args[1]
+	fmt.Println(utf8.RuneCountInString(name))
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Improved Banger
+//
+//  Change the Banger program the work with Unicode
+//  characters.
+//
+// INPUT
+//  "İNANÇ"
+//
+// EXPECTED OUTPUT
+//  İNANÇ!!!!!
+// ---------------------------------------------------------
+
+func improvedBanger() {
+	msg := os.Args[1]
+
+	s := msg + strings.Repeat("!", utf8.RuneCountInString(msg))
+
+	fmt.Println(s)
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Trim It
+//
+//  1. Look at the documentation of strings package
+//  2. Find a function that trims the spaces from
+//     the given string
+//  3. Trim the text variable and print it
+//
+// EXPECTED OUTPUT
+//  The weather looks good.
+//  I should go and play.
+// ---------------------------------------------------------
+
+func trimIt() {
+	msg := `
+	
+	The weather looks good.
+I should go and play.
+	`
+
+	msg = strings.TrimSpace(msg)
+	fmt.Println(msg)
+
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Right Trim It
+//
+//  1. Look at the documentation of strings package
+//  2. Find a function that trims the spaces from
+//     only the right-most part of the given string
+//  3. Trim it from the right part only
+//  4. Print the number of characters it contains.
+//
+// RESTRICTION
+//  Your program should work with unicode string values.
+//
+// EXPECTED OUTPUT
+//  5
+// ---------------------------------------------------------
+
+func rightTrim() {
+	// currently it prints 17
+	// it should print 5
+
+	name := "inanç           "
+	fmt.Println(utf8.RuneCountInString(strings.TrimRight(name, " ")))
 }
