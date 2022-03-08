8207dc909911bdf46f58f272399b7906eaf1016a
Brendan Ashton <brendan.ashton@getweave.com>
2022-03-08 08:05:48 -0700

    finish raw interpolation

--- 01-basics/stringsExercises/main.go
+++ 01-basics/stringsExercises/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	literal()
 	printJson()
+	rawConcat()
 }
 
 // ---------------------------------------------------------
@@ -74,4 +78,40 @@ func printJson() {
 
 	fmt.Println(json)
 	fmt.Println(rawjson)
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Raw Concat
+//
+//  1. Initialize the name variable
+//     by getting input from the command line
+//
+//  2. Use concatenation operator to concatenate it
+//     with the raw string literal below
+//
+// NOTE
+//  You should concatenate the name variable in the correct
+//  place.
+//
+// EXPECTED OUTPUT
+//  Let's say that you run the program like this:
+//   go run main.go inanç
+//
+//  Then it should output this:
+//   hi inanç!
+//   how are you?
+// ---------------------------------------------------------
+
+func rawConcat() {
+	// uncomment the code below
+	// name := "and get the name from the command-line"
+
+	// replace and concatenate the `name` variable
+	// after `hi ` below
+
+	name := os.Args[1]
+	msg := `hi ` + name + `!
+how are you?`
+
+	fmt.Println(msg)
 }
