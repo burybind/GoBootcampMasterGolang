417d75de458a3c61647ea1b829e0f5a05afa2823
Brendan Ashton <brendan.ashton@getweave.com>
2022-03-08 07:48:51 -0700

    finish json exercise

--- 01-basics/stringsExercises/main.go
+++ 01-basics/stringsExercises/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+func main() {
+	literal()
+	printJson()
+}
+
 // ---------------------------------------------------------
 // EXERCISE: Windows Path
 //
@@ -17,11 +22,7 @@ import "fmt"
 //  Only that it should use a raw string literal instead.
 // ---------------------------------------------------------
 
-func main() {
-	// HINTS:
-	// \\ equals to backslash character
-	// \n equals to newline character
-
+func literal() {
 	path := "c:\\program files\\duper super\\fun.txt\n" +
 		"c:\\program files\\really\\funny.png"
 	rawpath := `c:\program files\duper super\fun.txt
@@ -29,4 +30,48 @@ c:\program files\really\funny.png`
 
 	fmt.Println(path)
 	fmt.Println(rawpath)
+}
+
+// ---------------------------------------------------------
+// EXERCISE: Print JSON
+//
+//  1. Change the following program
+//  2. It should use a raw string literal instead
+//
+// HINT
+//  Run this program first to see its output.
+//  Then you can easily understand what it does.
+//
+// EXPECTED OUTPUT
+//  Your solution should output the same as this program.
+//  Only that it should use a raw string literal instead.
+// ---------------------------------------------------------
+
+func printJson() {
+	// HINTS:
+	// \t equals to TAB character
+	// \n equals to newline character
+	// \" equals to double-quotes character
+
+	json := "\n" +
+		"{\n" +
+		"\t\"Items\": [{\n" +
+		"\t\t\"Item\": {\n" +
+		"\t\t\t\"name\": \"Teddy Bear\"\n" +
+		"\t\t}\n" +
+		"\t}]\n" +
+		"}\n"
+
+	rawjson := `
+{
+	"Items": [{
+		"Item": {
+			"name": "Teddy Bear" 
+		}
+	}]
+}
+`
+
+	fmt.Println(json)
+	fmt.Println(rawjson)
 }
