ce14137a2d935a1fdfb9b6263d774094219d54ba
Brendan Ashton <brendan.ashton@getweave.com>
2022-03-10 06:04:19 -0700

    actually add iota changes to git

--- 01-basics/iota/main.go
+++ 01-basics/iota/main.go
@@ -1,1 +1,26 @@
-package iota
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		monday = iota +1
+		tuesday
+		wednesday
+		thursday
+		friday
+		saturday
+		sunday
+	)
+
+	fmt.Println(monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+
+	const (
+		EST = -(5 + iota)
+		_
+		MST = -7
+		PST = -8
+	)
+
+	fmt.Println(EST, MST, PST)
+}
